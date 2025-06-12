6e0b95ca396f18fdda3814dfe0414f18e9c6f96a
iotamale <kamperrro@gmail.com>
2025-06-12 20:01:13 +0200

    Add optiont to disable webhook logging

--- src/utils/webhookTransport.ts
+++ src/utils/webhookTransport.ts
@@ -4,58 +4,57 @@ import { EmbedBuilder } from 'discord.js';
 import { BaseEmbed } from '../types/messageTypes';
 
 interface WebhookTransportOptions {
-    webhookUrl: string;
-    level?: string;
+	webhookUrl: string;
+	level?: string;
 }
 
 class WebhookTransport extends TransportStream {
-    private webhookUrl: string;
+	private webhookUrl: string | null;
 
-    constructor(opts: WebhookTransportOptions) {
-        super(opts);
-        this.webhookUrl = opts.webhookUrl;
-    }
+	constructor(opts: WebhookTransportOptions) {
+		super(opts);
+		this.webhookUrl = opts.webhookUrl;
+	}
 
-    log(info: any, callback: () => void) {
-        setImmediate(() => {
-            this.emit('logged', info);
-        });
+	log(info: any, callback: () => void) {
+		if (!this.webhookUrl || this.webhookUrl === '' || this.webhookUrl === 'null' || this.webhookUrl === '-') return;
+		setImmediate(() => {
+			this.emit('logged', info);
+		});
 
-        const { level, message, timestamp } = info;
-        createLogEmbed(level, message, timestamp).then(embed => {
-            this.sendWebhook(embed, 0);
-        });
+		const { level, message, timestamp } = info;
+		createLogEmbed(level, message, timestamp).then((embed) => {
+			this.sendWebhook(embed, 0);
+		});
 
-        callback();
-    }
+		callback();
+	}
 
-    private async sendWebhook(embed: EmbedBuilder, retryCount: number) {
-        try {
-            await axios.post(this.webhookUrl, {
-                embeds: [embed.toJSON()]
-            });
-        } catch (error) {
-            if (axios.isAxiosError(error) && error.response && error.response.status === 429 && retryCount < 5) {
-                const retryAfter = error.response.headers['retry-after'] || 1;
-                setTimeout(() => {
-                    this.sendWebhook(embed, retryCount + 1);
-                }, retryAfter * 1000);
-            } else {
-                console.error('Error sending webhook:', error);
-            }
-        }
-    }
+	private async sendWebhook(embed: EmbedBuilder, retryCount: number) {
+		try {
+			if (!this.webhookUrl || this.webhookUrl === '' || this.webhookUrl === 'null' || this.webhookUrl === '-') return;
+			await axios.post(this.webhookUrl, {
+				embeds: [embed.toJSON()],
+			});
+		} catch (error) {
+			if (axios.isAxiosError(error) && error.response && error.response.status === 429 && retryCount < 5) {
+				const retryAfter = error.response.headers['retry-after'] || 1;
+				setTimeout(() => {
+					this.sendWebhook(embed, retryCount + 1);
+				}, retryAfter * 1000);
+			} else {
+				console.error('Error sending webhook:', error);
+			}
+		}
+	}
 }
 
 export default WebhookTransport;
 
 function createLogEmbed(level: string, message: string, timestamp: string): Promise<BaseEmbed> {
-    const embed = new BaseEmbed()
-        .setTitle('Log Message')
-        .setColor(level === 'error' ? 0xff0000 : 0x00ff00)
-        .addFields(
-            { name: 'Type', value: level, inline: true },
-            { name: 'Log', value: message }
-        );
-    return Promise.resolve(embed);
+	const embed = new BaseEmbed()
+		.setTitle('Log Message')
+		.setColor(level === 'error' ? 0xff0000 : 0x00ff00)
+		.addFields({ name: 'Type', value: level, inline: true }, { name: 'Log', value: message });
+	return Promise.resolve(embed);
 }

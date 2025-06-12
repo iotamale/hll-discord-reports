60c843837e696b8b2b99cb94bea19172dbbee4ab
iotamale <kamperrro@gmail.com>
2025-06-12 11:14:19 +0200

    Create CRCON API wrapper

--- src/crconApiClient.ts
+++ src/crconApiClient.ts
@@ -1,34 +1,94 @@
 import { getConfigVariable } from './utils/utils';
 import axios, { Axios, AxiosResponse } from 'axios';
 import logger from './logger';
-import { ExtendedClient } from './types/discordClientTypes';
-import { HllServerConfig } from './types/crconTypes';
+import { HllServerConfig, GamePlayerInfo, PlayerStats, Watchlist } from './types/crconTypes';
 
 export class CRCONClient {
-    public readonly hllServers: Array<HllServerConfig>;
-    private readonly headers: any;
-    private readonly rconApiKey: string = getConfigVariable('RCON_API_KEY');
+	public readonly hllServers: Array<HllServerConfig>;
+	private readonly headers: any;
+	private readonly rconApiKey: string = getConfigVariable('RCON_API_KEY');
 
-    constructor() {
-        this.hllServers = [];
-        const hllServersData = getConfigVariable('servers');
-        for (const serverData of hllServersData) {
-            const hllServer = new HllServerConfig(serverData.name, serverData.display_name, serverData.api_base_url, serverData.websocket_url, serverData.roles);
-            this.hllServers.push(hllServer);
-        }
-        this.headers = {
-            'authorization': `Bearer ${this.rconApiKey}`,
-            'content-type': 'application/json'
-        };
-    }
+	constructor() {
+		this.hllServers = [];
+		const hllServersData = getConfigVariable('servers');
+		for (const serverData of hllServersData) {
+			const hllServer = new HllServerConfig(serverData.name, serverData.display_name, serverData.api_base_url, serverData.websocket_url, serverData.roles);
+			this.hllServers.push(hllServer);
+		}
+		this.headers = {
+			authorization: `Bearer ${this.rconApiKey}`,
+			'content-type': 'application/json',
+		};
+	}
 
-    public async getGameState(url: string): Promise<AxiosResponse> {
-        try {
-            const response = await axios.get(`${url}/get_gamestate`, { headers: this.headers });
-            return response;
-        } catch (error) {
-            logger.error('Error fetching server info:', error);
-            throw new Error('Error fetching server info');
-        }
-    };
-};
+	public async getGameState(url: string): Promise<AxiosResponse> {
+		try {
+			const response = await axios.get(`${url}/get_gamestate`, { headers: this.headers });
+			return response;
+		} catch (error) {
+			logger.error('Error fetching server info:', error);
+			throw new Error('Error fetching server info');
+		}
+	}
+
+	public async getPlayerStats(url: string, playerName: string): Promise<PlayerStats | null> {
+		try {
+			const response = await axios.get(`${url}/get_live_game_stats`, { headers: this.headers });
+			if (response.status !== 200) {
+				logger.error(`Failed to fetch player stats for ${playerName} from ${url}. Status code: ${response.status}`);
+				throw new Error(`Failed to fetch player stats for ${playerName}`);
+			}
+			const data = response?.data?.result?.stats.filter((x: any) => x.player === playerName)[0];
+			if (!data) {
+				logger.warn(`No stats found for player ${playerName} on server ${url}`);
+				return null;
+			}
+			return new PlayerStats(
+				data.kills,
+				data.deaths,
+				data.teamkills,
+				data.kills_per_minute,
+				data.kills_streak,
+				data.deaths_without_kill_streak,
+				data.teamkills_streak
+			);
+		} catch (error) {
+			logger.error('Error fetching server info:', error);
+			throw new Error('Error fetching server info');
+		}
+	}
+
+	public async getPlayerInfo(url: string, playerName: string): Promise<GamePlayerInfo | null> {
+		// let response: AxiosResponse;
+		try {
+			const response = await axios.get(`${url}/get_detailed_player_info?player_name=${playerName}`, { headers: this.headers });
+			if (response.status !== 200) {
+				logger.error(`Failed to fetch player info for ${playerName} from ${url}. Status code: ${response.status}`);
+				throw new Error(`Failed to fetch player info for ${playerName}`);
+			}
+			const data = response?.data?.result;
+			const watchData = data?.profile?.watchlist;
+			const watchlist = watchData ? new Watchlist(watchData.reason, watchData.by) : null;
+
+			const playerStats = await this.getPlayerStats(url, playerName);
+			return new GamePlayerInfo(
+				data.name,
+				data.player_id,
+				data.profile.sessions_count,
+				data.profile.total_playtime_seconds,
+				data.is_vip,
+				data.level,
+				data.unit_name,
+				data.loadout,
+				data.team,
+				data.role,
+				watchlist,
+				playerStats,
+				data.profile.penalty_count || null
+			);
+		} catch (error) {
+			logger.error('Error fetching server info:', error);
+			throw new Error('Error fetching server info');
+		}
+	}
+}
--- src/types/crconTypes.ts
+++ src/types/crconTypes.ts
@@ -1,15 +1,104 @@
 export class HllServerConfig {
-    name: string;
-    displayName: string;
-    apiBaseUrl: string;
-    websocketUrl: string;
-    roles: string[];
+	name: string;
+	displayName: string;
+	apiBaseUrl: string;
+	websocketUrl: string;
+	roles: string[];
 
-    constructor(name: string, displayName: string, apiBaseUrl: string, websocketUrl: string, roles: string[]) {
-        this.name = name;
-        this.displayName = displayName;
-        this.apiBaseUrl = apiBaseUrl;
-        this.websocketUrl = websocketUrl;
-        this.roles = roles;
-    }
+	constructor(name: string, displayName: string, apiBaseUrl: string, websocketUrl: string, roles: string[]) {
+		this.name = name;
+		this.displayName = displayName;
+		this.apiBaseUrl = apiBaseUrl;
+		this.websocketUrl = websocketUrl;
+		this.roles = roles;
+	}
+}
+export class Watchlist {
+	reason: string;
+	by: string;
+
+	constructor(reason: string, by: string) {
+		this.reason = reason;
+		this.by = by;
+	}
+}
+
+export class PlayerStats {
+	kills: number;
+	deaths: number;
+	teamKills: number;
+	kpm: number;
+	killStreak: number;
+	deathStreak: number;
+	teamKillStreak: number;
+
+	constructor(
+		kills: number = 0,
+		deaths: number = 0,
+		teamKills: number = 0,
+		kpm: number = 0,
+		killStreak: number = 0,
+		deathStreak: number = 0,
+		teamKillStreak: number = 0
+	) {
+		this.kills = kills;
+		this.deaths = deaths;
+		this.teamKills = teamKills;
+		this.kpm = kpm;
+		this.killStreak = killStreak;
+		this.deathStreak = deathStreak;
+		this.teamKillStreak = teamKillStreak;
+	}
+}
+
+export class GamePlayerInfo {
+	playerName: string;
+	playerId: string;
+	sessionsCount: number;
+	totalPlayTimeSeconds: number;
+	isVip: boolean;
+	level: number;
+	unitName: string;
+	loadout: string;
+	team: string;
+	role: string;
+	watchlist: Watchlist | null;
+	stats: PlayerStats | null;
+	penaltyCount: Object | null;
+	/*"penalty_count": {
+        "KICK": 0,
+        "PUNISH": 56,
+        "TEMPBAN": 0,
+        "PERMABAN": 0
+    }*/
+
+	constructor(
+		playerName: string,
+		playerId: string,
+		sessionsCount: number,
+		totalPlayTimeSeconds: number,
+		isVip: boolean,
+		level: number,
+		unitName: string,
+		loadout: string,
+		team: string,
+		role: string,
+		watchlist: Watchlist | null = null,
+		playerStats: PlayerStats | null = null,
+		penaltyCount: Object | null = null
+	) {
+		this.playerName = playerName;
+		this.playerId = playerId;
+		this.sessionsCount = sessionsCount;
+		this.totalPlayTimeSeconds = totalPlayTimeSeconds;
+		this.isVip = isVip;
+		this.level = level;
+		this.unitName = unitName;
+		this.loadout = loadout;
+		this.team = team;
+		this.role = role;
+		this.watchlist = watchlist;
+		this.stats = playerStats;
+		this.penaltyCount = penaltyCount;
+	}
 }
